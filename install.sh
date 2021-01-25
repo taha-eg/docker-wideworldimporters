380bd4858df19953210bc665870c4c47f52a6d03
Taha Mohamed <taha.mohamed@baytak.com>
2021-01-25 14:52:36 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Running installation script..."
+
+#wait for the SQL Server to come up
+sleep 30s
+
+#run the setup script to create the DB and the schema in the DB
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i install.sql
+echo
+echo "Done installing database."
+echo "Server is ready."
+sleep infinity
